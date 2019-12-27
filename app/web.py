f1bed213d84a9ec7e9cccac1b58bf31a15ee3ad7
Shiv Gowda <shivarajugowda@users.noreply.github.com>
2019-12-26 23:15:09 -0800

    small update

--- app/web.py
+++ app/web.py
@@ -1,27 +1,39 @@
 from fastapi import FastAPI
-from app.worker import runQuery, rclient
-from app.jsonData import getQueuedStatus
+import app.worker as worker
 from starlette.requests import Request
 from starlette.responses import Response
-
+import app.config as config
 import gzip
 
-app = FastAPI()
+fastApi = FastAPI()
 
-@app.get("/ping")
+@fastApi.get("/ping")
 async def ping():
     return 'pong!'
 
-@app.post("/v1/statement")
+@fastApi.post("/v1/statement")
 async def query(request: Request):
     body = await request.body()
     sql: str = bytes.decode(body)
-    task = runQuery.delay(sql)
-    return getQueuedStatus(task.id)
+    user = request.headers.get('X-Presto-User')
 
-@app.get("/v1/statement/{state}/{queryId}/{token}/{page}")
+    if not user:
+        raise AssertionError('User name must be specified')
+
+    taskId = worker.runPrestoJob(user, sql)
+    return config.getQueuedStatus(taskId)
+
+@fastApi.get("/v1/statement/{state}/{queryId}/{token}/{page}")
 async def status(state : str, queryId : str, token : str, page : str):
-    if rclient.hget(queryId, "DONE") :
-        data = gzip.decompress(rclient.hget(queryId, page)).decode()
+    ## Block execution till work is complete. Retry becomes easy.
+    if config.rclient.hexists(queryId, "DONE") :
+        data = gzip.decompress(config.rclient.hget(queryId, page)).decode()
         return Response(content=data, media_type="application/json")
-    return getQueuedStatus(queryId)
+
+    ## Stream results as they become available.
+    # data = config.rclient.hget(queryId, page)
+    # if data:
+    #     data = gzip.decompress(data).decode()
+    #     return Response(content=data, media_type="application/json")
+
+    return config.getExecutingStatus(queryId,page)
